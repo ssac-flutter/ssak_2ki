4bff5b954de5f117aa2472049133b588f95c1002
Junsuk Oh <a811219@gmail.com>
2021-09-24 16:05:38 +0900

    http, then 활용

---
+++ examples/network_sample/lib/ui/home_page.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+
+class HomePage extends StatefulWidget {
+  const HomePage({Key key}) : super(key: key);
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  var _result = '';
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('네트워크 통신'),
+      ),
+      body: Column(
+        children: [
+          ElevatedButton(
+            onPressed: () {
+              http
+                  .get('https://jsonplaceholder.typicode.com/todos/1')
+                  .then((response) {
+                print(response.statusCode);
+                print(response.body);
+
+                Map<String, dynamic> jsonResponse = jsonDecode(response.body);
+                String title = jsonResponse['title'];
+
+                print(title);
+
+                setState(() {
+                  _result = title;
+                });
+
+              });
+            },
+            child: Text('가져오기'),
+          ),
+          Text(_result),
+        ],
+      ),
+    );
+  }
+}
