30f9e42bc9b96c0f5c5bd434f2297f607ccec238
Junsuk Oh <a811219@gmail.com>
2021-10-05 15:52:19 +0900

    FutureBuilder 활용한 코드로 수정

--- examples/network_sample/lib/ui/home_page.dart
+++ examples/network_sample/lib/ui/home_page.dart
@@ -1,41 +1,11 @@
-import 'package:flutter/material.dart';
-import 'package:http/http.dart' as http;
 import 'dart:convert';
 
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
 import 'package:network_sample/model/todo.dart';
 
-class HomePage extends StatefulWidget {
-  const HomePage({Key key}) : super(key: key);
+class HomePage extends StatelessWidget {
 
-  @override
-  State<HomePage> createState() => _HomePageState();
-}
-
-class _HomePageState extends State<HomePage> {
-  var _result = '';
-  List<Todo> _list = [];
-
-  // 최초에 로드 될 때 호출
-  @override
-  void initState() {
-    super.initState();
-
-    init();
-  }
-
-  Future<void> init() async {
-    Todo todo = await fetch();
-    setState(() {
-      _result = todo.title;
-    });
-
-    List<Todo> todos = await fetchList();
-    setState(() {
-      _list = todos;
-    });
-  }
-
-  // initState -> ? -> ? -> build
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -47,23 +17,64 @@ class _HomePageState extends State<HomePage> {
           ElevatedButton(
             onPressed: () async {
               Todo todo = await fetch();
-              setState(() {
-                _result = todo.title;
-              });
+              // setState(() {
+                // _result = todo.title;
+              // });
             },
             child: Text('가져오기'),
           ),
-          Text(_result),
+          FutureBuilder<Todo>(
+              future: fetch(),
+              builder: (context, AsyncSnapshot<Todo> snapshot) {
+                if (snapshot.connectionState == ConnectionState.waiting) {
+                  return Center(child: CircularProgressIndicator());
+                }
+
+                if (snapshot.hasError) {
+                  return Text('에러가 발생했습니다');
+                }
+
+                if (!snapshot.hasData) {
+                  return Text('데이터가 없습니다');
+                }
+
+                Todo todo = snapshot.data;
+
+                return Text(todo.title);
+              }),
           ElevatedButton(
             onPressed: () async {
               List<Todo> todos = await fetchList();
-              setState(() {
-                _list = todos;
-              });
+              // setState(() {
+                // _list = todos;
+              // });
             },
             child: Text('목록 가져오기'),
           ),
-          if (_list.isEmpty) Center(child: CircularProgressIndicator()) else ..._list.map((e) => Text(e.title)).toList(),
+          FutureBuilder<List<Todo>>(
+              future: fetchList(),
+              initialData: [],
+              builder: (context, snapshot) {
+                if (snapshot.connectionState == ConnectionState.waiting) {
+                  return Center(child: CircularProgressIndicator());
+                }
+
+                if (snapshot.hasError) {
+                  return Text('에러가 발생했습니다');
+                }
+
+                if (!snapshot.hasData) {
+                  return Text('데이터가 없습니다');
+                }
+
+                List<Todo> todos = snapshot.data;
+
+                return ListView(
+                  shrinkWrap: true,
+                  physics: NeverScrollableScrollPhysics(),
+                  children: todos.map((e) => Text(e.title)).toList(),
+                );
+              }),
         ],
       ),
     );
