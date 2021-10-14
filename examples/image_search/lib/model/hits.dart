213349afaa3b556316ee763432d4d25dafce9a00
Junsuk Oh <a811219@gmail.com>
2021-10-14 16:43:30 +0900

    Hits 에 freezed 적용

--- examples/image_search/lib/model/hits.dart
+++ examples/image_search/lib/model/hits.dart
@@ -1,15 +1,15 @@
 import 'package:json_annotation/json_annotation.dart';
+import 'package:freezed_annotation/freezed_annotation.dart';
+
+part 'hits.freezed.dart';
 
 part 'hits.g.dart';
 
-@JsonSerializable()
-class Hits {
-  String previewURL;
-  String tags;
-
-  Hits(this.previewURL, this.tags);
+@freezed
+class Hits with _$Hits {
+  factory Hits({
+    required String previewURL, required String tags,
+  }) = _Hits;
 
   factory Hits.fromJson(Map<String, dynamic> json) => _$HitsFromJson(json);
-
-  Map<String, dynamic> toJson() => _$HitsToJson(this);
 }
