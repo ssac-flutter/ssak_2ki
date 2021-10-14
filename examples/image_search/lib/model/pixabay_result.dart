b426fa5682f294915262f8bfba096960392ad9b4
Junsuk Oh <a811219@gmail.com>
2021-10-14 16:29:14 +0900

    json_serializable 적용

--- examples/image_search/lib/model/pixabay_result.dart
+++ examples/image_search/lib/model/pixabay_result.dart
@@ -1,32 +1,17 @@
-import 'hits.dart';
+import 'package:image_search/model/hits.dart';
+import 'package:json_annotation/json_annotation.dart';
 
+part 'pixabay_result.g.dart';
+
+@JsonSerializable()
 class PixabayResult {
-  late int total;
-  late int totalHits;
-  late List<Hits> hits;
+  int total;
+  int totalHits;
+  List<Hits> hits;
 
-  PixabayResult({
-      required this.total,
-      required this.totalHits,
-      required this.hits,});
+  PixabayResult(this.total, this.totalHits, this.hits);
 
-  PixabayResult.fromJson(Map<String, dynamic> json) {
-    total = json['total'];
-    totalHits = json['totalHits'];
-    if (json['hits'] != null) {
-      hits = [];
-      json['hits'].forEach((v) {
-        hits.add(Hits.fromJson(v));
-      });
-    }
-  }
+  factory PixabayResult.fromJson(Map<String, dynamic> json) => _$PixabayResultFromJson(json);
 
-  Map<String, dynamic> toJson() {
-    final map = <String, dynamic>{};
-    map['total'] = total;
-    map['totalHits'] = totalHits;
-    map['hits'] = hits.map((v) => v.toJson()).toList();
-    return map;
-  }
-
+  Map<String, dynamic> toJson() => _$PixabayResultToJson(this);
 }
