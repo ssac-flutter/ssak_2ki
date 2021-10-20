a9dffebc33422a0e7c0aa671af7499c77624ce69
Junsuk Oh <a811219@gmail.com>
2021-10-20 17:53:09 +0900

    ViewModel 테스트 코드 작성

---
+++ examples/state_management/test/stream/stream_sample_view_model_test.dart
@@ -0,0 +1,27 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:state_management/stream/stream_sample_view_model.dart';
+
+void main() {
+  test('Stream이 잘 동작해야 한다', () async {
+    final viewModel = StreamSampleViewModel();
+
+    viewModel.countIncrement();
+    Future.delayed(const Duration(seconds: 1));
+
+    viewModel.countIncrement();
+    Future.delayed(const Duration(seconds: 1));
+
+    viewModel.countIncrement();
+    Future.delayed(const Duration(seconds: 1));
+
+    expect(
+      viewModel.countStream,
+      emitsInOrder([
+        isA<int>(),
+        isA<int>(),
+        isA<int>(),
+        isA<int>(),
+      ]),
+    );
+  });
+}
